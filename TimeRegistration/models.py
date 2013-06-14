20b698a4084979f193e7da5204882149acb84965
Stephan Telling <st@itelling.dk>
2013-06-14 11:38:18 +0200

    Initial commit

---
+++ TimeRegistration/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    employee_id = models.IntegerField()
+    department = models.CharField(max_length=100)
+    employment_date = models.DateField()
+
+    def __unicode__(self):
+        return self.user.username
+
+
+class TimeRegistration(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateTimeField()
+    hours = models.FloatField()
+
+    def __unicode__(self):
+        return self.hours
