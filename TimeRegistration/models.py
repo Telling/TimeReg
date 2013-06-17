200a11013672b9af9a771122f081bd8abe995c58
Stephan Telling <st@itelling.dk>
2013-06-17 14:28:11 +0200

    Added project model

--- TimeRegistration/models.py
+++ TimeRegistration/models.py
@@ -16,6 +16,17 @@ class TimeRegistration(models.Model):
     user = models.ForeignKey(User)
     date = models.DateTimeField()
     hours = models.FloatField()
+    project = models.OneToOneField('Project')
 
     def __unicode__(self):
         return unicode(self.hours)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_id = models.IntegerField()
+    project_manager = models.ForeignKey(User, null=True, blank=True)
+    project_desc = models.CharField(max_length=200, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.project_id
