53b72a96307d74561f70424e9b01c98cba4af4f0
Stephan Telling <st@itelling.dk>
2013-07-15 20:56:02 +0200

    Added Project to admin site

--- TimeRegistration/admin.py
+++ TimeRegistration/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, TimeRegistration
+from .models import Profile, TimeRegistration, Project
 
 admin.site.register(Profile)
 admin.site.register(TimeRegistration)
+admin.site.register(Project)
