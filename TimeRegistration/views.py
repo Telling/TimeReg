6fa9a407ecb08a99d4e5d97e41396f15f55e0185
Stephan Telling <st@itelling.dk>
2013-09-26 14:50:08 +0200

    Fixed views for timereg and projects, added admin tools

--- TimeRegistration/views.py
+++ TimeRegistration/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.template import RequestContext
 from django.contrib import messages
-from TimeRegistration.models import TimeRegistration, Project
-from django.db.models import Sum, Avg
+from django.utils import timezone
+from django.db.models import Max, Sum
+from TimeRegistration.models import TimeRegistration
+from TimeRegistration.forms import TimeRegForm, ProjectRegForm
+from datetime import date
 
 
 def login_user(request):
@@ -26,76 +29,6 @@ def login_user(request):
     return redirect('/')
 
 
-def home_statistics(request):
-    context = {}
-    if request.user.is_authenticated():
-        timeregistrations = TimeRegistration.objects.filter(
-            user=request.user)
-
-        context['timeregistrations'] = timeregistrations.order_by('-hours')
-
-        projects = Project.objects.filter(
-            timeregistration__user=request.user).annotate(
-                total_hours=Sum('timeregistration__hours')
-            ).order_by('-total_hours')
-
-        context['projects'] = projects
-
-        # Calculate the average daily hours
-        def average_daily_hours():
-            average_pr_day = timeregistrations \
-                .extra(select={
-                    'd': 'extract(day FROM date)',
-                    'm': 'extract(month FROM date)',
-                    'y': 'extract(year FROM date)'
-                }) \
-                .values('d', 'm', 'y') \
-                .annotate(avg_hours=Avg('hours'))
-
-            total_hours_by_day = 0.0
-            for i in range(len(average_pr_day)):
-                total_hours_by_day += average_pr_day[i]['avg_hours']
-
-            return total_hours_by_day / len(average_pr_day)
-
-        context['average_daily'] = average_daily_hours()
-
-        # Calculate the average weekly hours
-        def average_weekly_hours():
-            average_pr_week = timeregistrations \
-                .extra(select={'y': 'extract(year FROM date)'}) \
-                .values('week', 'y') \
-                .annotate(avg_hours=Avg('hours'))
-
-            total_hours_by_week = 0.0
-            for i in range(len(average_pr_week)):
-                total_hours_by_week += average_pr_week[i]['avg_hours']
-
-            return total_hours_by_week / len(average_pr_week)
-
-        context['average_weekly'] = average_weekly_hours()
-
-        # Calculate the average monthly hours
-        def average_monthly_hours():
-            average_pr_month = timeregistrations \
-                .extra(select={
-                    'm': 'extract(month FROM date)',
-                    'y': 'extract(year FROM date)'
-                }) \
-                .values('m', 'y') \
-                .annotate(avg_hours=Avg('hours'))
-
-            total_hours_by_month = 0.0
-            for i in range(len(average_pr_month)):
-                total_hours_by_month += average_pr_month[i]['avg_hours']
-
-            return total_hours_by_month / len(average_pr_month)
-
-        context['average_monthly'] = average_monthly_hours()
-
-    return render_to_response('index.html', RequestContext(request, context))
-
-
 def logout_user(request):
     logout(request)
     messages.success(request, 'You\'ve been logged out successfully!')
@@ -107,11 +40,61 @@ def account(request):
 
 
 def projects(request):
-    return render_to_response('projects.html', RequestContext(request))
+    context = {}
+
+    projects = request.user.projects.all().annotate(
+        total_hours=Sum('timeregistration__hours')).order_by('project_id')
+
+    open_projects = projects.filter(is_active=True)
+    context['open_projects'] = open_projects
+
+    closed_projects = projects.filter(is_active=False)
+    context['closed_projects'] = closed_projects
+
+    return render_to_response('projects.html',
+                              RequestContext(request, context))
 
 
-def time_registration(request):
+def registration_view(request):
     return render_to_response('timeregistration.html', RequestContext(request))
+
+
+def time_registration(request, weeknumber=None):
+    if request.user.is_authenticated():
+        context = {}
+
+        today = timezone.now()
+        context['current_date'] = '{}-{:0>2}-{:0>2}'.format(
+            today.year, today.month, today.day)
+
+        if weeknumber is None:
+            registrations = TimeRegistration.objects.filter(
+                user=request.user, week=date.today().isocalendar()[1]
+            ).order_by('-date')
+        else:
+            registrations = TimeRegistration.objects.filter(
+                user=request.user, week=weeknumber).order_by('-date')
+
+        context['registrations'] = registrations
+
+        if request.method == "POST":
+            form = TimeRegForm(request.POST)
+            if form.is_valid():
+                timeregistration = form.save(commit=False)
+                timeregistration.user = request.user
+                timeregistration.week = form.cleaned_data[
+                    'date'].isocalendar()[1]
+                timeregistration.save()
+                return redirect('/')
+        else:
+            form = TimeRegForm()
+
+        context['form'] = form
+
+        return render_to_response('index.html',
+                                  RequestContext(request, context))
+    else:
+        return render_to_response('index.html', RequestContext(request))
 
 
 def tools_users(request):
@@ -119,4 +102,27 @@ def tools_users(request):
 
 
 def tools_projects(request):
-    return render_to_response('tools_projects.html', RequestContext(request))
+    context = {}
+
+    def next_project_id():
+        current_id = projects.aggregate(Max('project_id'))['project_id__max']
+        if current_id is None:
+            return 0
+        else:
+            return current_id
+
+    if request.method == "POST":
+        form = ProjectRegForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.project_id = next_project_id() + 1
+            project.is_active = True
+            project.save()
+            form.save_m2m()
+            return redirect('/tools/projects/')
+    else:
+        form = ProjectRegForm()
+
+    context['project_form'] = form
+    return render_to_response('tools_projects.html',
+                              RequestContext(request, context))
